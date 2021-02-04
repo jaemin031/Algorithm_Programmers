684517cabe2506873947e87c1356b2ee93b8a487
jaemin031 <34496139+jaemin031@users.noreply.github.com>
2021-02-04 18:58:09 +0900

    [JAVA][찾아라 프로그래밍 마에스터] 폰켓몬

---
+++ 프로그래머스/prgrms_programming_meister01.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        
+        for(int n : nums)
+        	set.add(n);        	
+            
+        return set.size()>=nums.length/2 ? nums.length/2 : set.size();
+    }
+}
